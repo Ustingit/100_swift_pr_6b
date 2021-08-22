97971e61cfbaa9e750920dbc9e6909f578f622ea
Yuryi Ustsinovich <ustinhtc@gmail.com>
2021-08-22 18:00:14 +0300

    Initial Commit

---
+++ Project6c/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6c
+//
+//  Created by Yuryi Ustinovich on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
